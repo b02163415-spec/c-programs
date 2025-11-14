00903e0c2da9b333932fa3cc1478561e3e9b256a
b02163415-spec <b02163415@gmail.com>
2025-11-14 11:09:42 +0530

    Add files via upload

---
+++ OPERATOR ALL CASES.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	char op;
+	double first , second;
+	printf("enter an operator(+,-,*,/):");
+	scanf("%c, &op");
+	printf("enter two operands:");
+	scanf("%if%if",&first, &second);
+	switch(op){
+		case'+':
+			printf("%.1if+%.1if=%.1if",first,second,first+second);
+			break;
+		case'-'	:
+			printf("%.1if-%.1if=%.1if",first,second,first-second);
+			break;
+		case'*':
+			printf("%.1if*%.1if=%.1if",first,second,first*second);
+			break;
+		case'/':
+			printf("%.1if/%.1if=%.1if",first,second,first/second);
+			break;
+		//operator doesn't match any case constant
+		default:
+		printf("error ! operator is not correct");
+	}
+	return 0;	
+}
