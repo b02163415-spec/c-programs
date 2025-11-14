c664c21cfd5526f52a256691ba0d2a80955a1077
b02163415-spec <b02163415@gmail.com>
2025-11-14 11:11:44 +0530

    Add files via upload

---
+++ palinmeter.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int num,rev,rem,temp;
+	printf("enter a 5 digits num: ");
+	scanf("%d",&num);
+	trap=0;
+    rev=0;
+    while(num!=0)
+    {
+    	rem=num%10;
+    	rev=rev*10+r;
+    	num=num/10;
+	}	
+}
