99035290177f83ba28609ea32bb3705f83ce0b7c
b02163415-spec <b02163415@gmail.com>
2025-11-14 11:12:38 +0530

    Add files via upload

---
+++ RECTANGLE.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float length ,breadth, area ;
+	printf("enter the value of length: ");
+    scanf("%f",&length);
+    printf("enter the value of breadth; ");
+    scanf("%f",& breadth);
+    area=length*breadth;
+    printf("area of rectangle:%f",area);
+	return 0;
+}
