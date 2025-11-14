fedbace15ede22a157dac0b482ed5caeff91434d
b02163415-spec <b02163415@gmail.com>
2025-11-14 11:10:55 +0530

    Add files via upload

---
+++ operators.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	int b;
+	printf("enter the value of a:");
+	scanf("%d,&a");
+	printf("enter the valu of b:");
+	scanf("%d,&b");
+	//performing operations on a and b
+	printf("asuming a is less than b:%d",a<b);
+
+	return 0;
+}
