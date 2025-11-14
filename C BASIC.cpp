87b371e55585db703ca262d0cd570b73623b3b8c
b02163415-spec <b02163415@gmail.com>
2025-11-14 10:35:27 +0530

    Add files via upload

---
+++ C BASIC.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a=5;
+	float b=65.94;
+	char grade;
+	char name[7];
+	printf("enter a");
+	scanf("%d",&num);
+	printf("enter b value":);
+	scanf("%f",&b);
+	printf("enter grade");
+	scanf("%c"/,&grade);
+	printf("enter name");
+	scanf("%s",name);
+	printf("%d|n%f\nc%\n%s,num,n,grade,name");
+	return 0;
+}
